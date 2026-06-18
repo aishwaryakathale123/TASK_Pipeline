f42794d1b91a9819cfde843a2bf2d35cd41b074a
Aishwarya <aishwaryakathale6896@gmail.com>
2026-06-18 17:05:42 +0530

    initial commit

---
+++ tests/TASK.spec.js
@@ -0,0 +1,14 @@
+import {test} from '@playwright/test'
+
+//playwright will bydefault cancel the alert
+
+test('dialogs',async ({page}) => {
+
+    await page.goto('https://testautomationpractice.blogspot.com')
+    await page.getByRole('buutton',{name:'Simple Alert'}).click()
+
+    await page.getByRole('button',{name:'Confirmation Alert'}).click()
+    
+    await page.getByRole('button',{name:'Prompt Alert'}).click()
+   
+})
